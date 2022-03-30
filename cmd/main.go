5cefb34f2e67abf8719a37942b5034f3baa9112c
KoLLlaka <kotshredengera@gmail.com>
2022-03-30 04:14:46 +0300

    func main and more folders

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/KoLLlaka/todo-app/internal/todo"
+)
+
+const (
+	port = "8080"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run(port); err != nil {
+		log.Fatalf("error occured while running http server: %s\n", err.Error())
+	}
 }
