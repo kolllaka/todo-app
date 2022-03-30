e4a853f6be1082ee722eaccbf222d5bb3967b8f8
KoLLlaka <kotshredengera@gmail.com>
2022-03-30 05:13:25 +0300

    viper and file config

--- cmd/main.go
+++ cmd/main.go
@@ -7,19 +7,27 @@ import (
 	"github.com/KoLLlaka/todo-app/pkg/handler"
 	"github.com/KoLLlaka/todo-app/pkg/repository"
 	"github.com/KoLLlaka/todo-app/pkg/service"
-)
-
-const (
-	port = "8080"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initialization configs: %s\n", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(port, handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s\n", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
