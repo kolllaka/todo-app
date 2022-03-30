529df15e68265080b1442817c4719191c52a684c
KoLLlaka <kotshredengera@gmail.com>
2022-03-30 11:13:55 +0300

    signUp handler

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -16,7 +16,15 @@ func (h *Handler) signUp(c *gin.Context) {
 		return
 	}
 
-	
+	id, err := h.services.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponce(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) signIn(c *gin.Context) {
