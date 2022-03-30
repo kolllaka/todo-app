1b6d09712220a49f922edf2542b7883d616326d6
KoLLlaka <kotshredengera@gmail.com>
2022-03-30 10:51:23 +0300

    hash passwords

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -1,8 +1,15 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
+
 	"github.com/KoLLlaka/todo-app/internal/todo"
 	"github.com/KoLLlaka/todo-app/pkg/repository"
+)
+
+const (
+	salt = "hjhk213haejhw"
 )
 
 type AuthService struct {
@@ -14,6 +21,14 @@ func newAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	
 	return s.repo.CreateUser(user)
 }
 
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
