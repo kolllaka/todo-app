1ba08aa8c3113f4ada31c25cea6d0760065221f3
KoLLlaka <kotshredengera@gmail.com>
2022-03-30 05:31:21 +0300

    description shema.sql

---
+++ shema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users
+(
+	id SERIAL NOT NULL UNIQUE,
+	name VARCHAR(255) NOT NULL,
+	username VARCHAR(255) NOT NULL UNIQUE,
+	pasword_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE todo_lists
+(
+	id SERIAL NOT NULL UNIQUE,
+	title VARCHAR(255) NOT NULL,
+	description VARCHAR(255)
+);
+
+CREATE TABLE users_lists
+(
+	id SERIAL NOT NULL UNIQUE,
+	user_id INT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+	list_id INT NOT NULL REFERENCES todo_lists (id) ON DELETE CASCADE 
+);
+
+CREATE TABLE todo_items
+(
+	id SERIAL NOT NULL UNIQUE,
+	title VARCHAR(255) NOT NULL,
+	description VARCHAR(255),
+	done BOOLEAN NOT NULL DEFAULT false
+);
