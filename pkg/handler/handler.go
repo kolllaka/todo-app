a5ead86f65964fa7ae68c27b4de519c280993fa6
KoLLlaka <kotshredengera@gmail.com>
2022-03-31 12:23:15 +0300

    remanage endpoints

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,7 +2,7 @@ package handler
 
 import (
 	"github.com/KoLLlaka/todo-app/pkg/service"
-	
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,15 +32,19 @@ func (h *Handler) InitRoutes() *gin.Engine {
 			lists.GET("/:id", h.getListById)
 			lists.PUT("/:id", h.updateList)
 			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItem)
+			}
 		}
 
-		items := lists.Group(":id/items")
+		items := api.Group("items")
 		{
-			items.POST("/", h.createItem)
-			items.GET("/", h.getAllItem)
-			items.GET("/:item_id", h.getItemById)
-			items.PUT("/:item_id", h.updateItem)
-			items.DELETE("/:item_id", h.deleteItem)
+			items.GET("/:id", h.getItemById)
+			items.PUT("/:id", h.updateItem)
+			items.DELETE("/:id", h.deleteItem)
 		}
 	}
 
